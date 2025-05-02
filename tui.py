50a7e5e841d9101da195440c3549081fdb9bd734
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-05-02 17:33:58 +0800

    commit to save work xd

---
+++ tui.py
@@ -0,0 +1,193 @@
+import os
+import updater
+
+if not (os.path.exists("lookups")):
+    print("downloading lookup files")
+    updater.update(
+        "https://update.structuralab.com/structuraUpdate", "Structura1-6", ""
+    )
+
+import json
+from structura_core import structura
+from numpy import array, int32, minimum
+import nbtlib
+from textual import on, work
+from textual.app import App, ComposeResult
+from textual.containers import HorizontalGroup, VerticalGroup
+from textual.widgets import Button, Input, Header, Footer, Label, Checkbox, Static
+from textual_fspicker import FileOpen, FileSave, Filters
+from textual.validation import Function
+from textual_slider import Slider
+
+debug = False
+
+
+def is_float(value: str) -> bool:
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
+class StructuraApp(App):
+    BINDINGS = []
+    CSS_PATH = "structura_tui.tcss"
+
+    def compose(self) -> ComposeResult:
+        yield Header("Structura")
+        yield VerticalGroup(
+            HorizontalGroup(
+                Label("Structure File", classes="label"),
+                Input(
+                    id="structure_file_location",
+                    placeholder="Path to structure file",
+                    validators=[
+                        Function(
+                            os.path.exists, failure_description="File does not exist"
+                        )
+                    ],
+                ),
+                Button(
+                    "Browse",
+                    variant="primary",
+                    id="browse_structure_file",
+                    classes="fsBrowser",
+                ),
+            ),
+            HorizontalGroup(
+                Label("Icon File", classes="label"),
+                Input(
+                    id="icon_file_location",
+                    placeholder="Path to icon file",
+                    validators=[
+                        Function(
+                            os.path.exists, failure_description="File does not exist"
+                        )
+                    ],
+                ),
+                Button(
+                    "Browse",
+                    variant="primary",
+                    id="browse_icon_file",
+                    classes="fsBrowser",
+                ),
+            ),
+            HorizontalGroup(
+                Label("Pack Name", classes="label"),
+                Input(id="pack_name", placeholder="Name of the pack"),
+                Static(classes="filler"),
+            ),
+            VerticalGroup(
+                HorizontalGroup(
+                    Label("Name Tag", classes="label"),
+                    Input(
+                        id="name_tag",
+                        placeholder="Name required on the armor stand to display the model",
+                    ),
+                ),
+                VerticalGroup(
+                    Static("Offset", classes="center-label"),
+                    HorizontalGroup(
+                        Input(
+                            id="offset_x",
+                            classes="offsets",
+                            placeholder="X Offset",
+                            value="0.0",
+                            validators=[
+                                Function(
+                                    is_float, failure_description="Not a floating point"
+                                )
+                            ],
+                        ),
+                        Input(
+                            id="offset_y",
+                            classes="offsets",
+                            placeholder="Y Offset",
+                            value="0.0",
+                            validators=[
+                                Function(
+                                    is_float, failure_description="Not a floating point"
+                                )
+                            ],
+                        ),
+                        Input(
+                            id="offset_z",
+                            classes="offsets",
+                            placeholder="Z Offset",
+                            value="0.0",
+                            validators=[
+                                Function(
+                                    is_float, failure_description="Not a floating point"
+                                )
+                            ],
+                        ),
+                    ),
+                ),
+                HorizontalGroup(
+                    Label("Transparency", classes="label"),
+                    Label("0%", classes="sliderLabel"),
+                    Slider(min=0, max=100, step=1, id="model_transparency"),
+                    Label("100%", classes="sliderLabel"),
+                ),
+                id="advanced_options",
+                classes="advanced",
+            ),
+            HorizontalGroup(
+                Checkbox("Advanced", id="advancedToggle"),
+                Checkbox("Make Lists", id="makeLists"),
+                Checkbox("Big Build mode", id="bigBuildMode", classes="advanced"),
+                Static(classes="fillerLarger"),
+                Button("Make Pack", variant="primary", id="makePack"),
+            ),
+            id="root",
+        )
+        yield Footer()
+
+    def on_mount(self) -> None:
+        advanced_elements = self.query(".advanced")
+        for advanced_element in advanced_elements:
+            advanced_element.styles.display = "none"
+
+    async def open_file(self, input_id: str, file_filter: Filters) -> None:
+        if opened := await self.push_screen_wait(
+            FileOpen(
+                must_exist=False,
+                filters=file_filter,
+            )
+        ):
+            self.query_one(f"#{input_id}", Input).value = str(opened)
+
+    @on(Button.Pressed, "#browse_structure_file")
+    @work
+    async def handle_browse_structure_file(self) -> None:
+        await self.open_file(
+            input_id="structure_file_location",
+            file_filter=Filters(
+                ("MCStructure", lambda f: f.suffix.lower() == ".mcstructure"),
+            ),
+        )
+
+    @on(Button.Pressed, "#browse_icon_file")
+    @work
+    async def handle_browse_icon_file(self) -> None:
+        await self.open_file(
+            input_id="icon_file_location",
+            file_filter=Filters(
+                ("PNG", lambda f: f.suffix.lower() == ".png"),
+            ),
+        )
+
+    @on(Checkbox.Changed, "#advancedToggle")
+    @work
+    async def handle_advanced_toggle(self) -> None:
+        advanced_elements = self.query(".advanced")
+        for advanced_element in advanced_elements:
+            advanced_element.styles.display = (
+                "block" if self.query_one("#advancedToggle").value else "none"
+            )
+
+
+if __name__ == "__main__":
+    app = StructuraApp()
+    app.run()
